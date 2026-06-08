8320cbcd8445f176074c59b559dc61a041eb2f83
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-08 18:31:32 +0530

    practice

---
+++ DEMO.java
@@ -0,0 +1,11 @@
+package NEW;
+
+public class DEMO {
+    public static void main(String[] args) {
+        System.out.println("HELLOWORD");
+        System.out.println("WELCOME TO JAVA SERIES ");
+        System.out.println("your author is neha deshpande");
+        
+    }
+    
+}
---
+++ Datatype.java
@@ -0,0 +1,24 @@
+//primitive data type 
+package NEW;
+public class Datatype {
+	public static void main(String []args) {
+		int a = 19;
+		float f = 19.5f;
+		boolean isgirl = false;
+		String name = "Neha";
+		byte m = (byte) 30000;
+		short length = 15;
+		char initial = 'N';
+		long mountain = 16000;
+		double salary = 50000;
+		System.out.println(a);
+		System.out.println(f);
+		System.out.println(isgirl + "isgirl");
+		System.out.println(name);
+		System.out.println(m);
+		System.out.println(length);
+		System.out.println(initial);
+		System.out.println(mountain);
+		System.out.println(salary);
+	}
+}
