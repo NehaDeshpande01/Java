af16351cbc0c1b4aef94f3102c54669a794f1c37
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-08 18:51:46 +0530

    range.java

---
+++ range.java
@@ -0,0 +1,20 @@
+//to find range of primitive data type
+package NEW;
+public class range {
+	public static void main (String [] args) {
+		System.out.println("Minimum range of byte: " +Byte.MIN_VALUE);
+		System.out.println("MAXIMUM range of byte: " +Byte.MAX_VALUE);
+		System.out.println("Minimum range of short: " +Short.MIN_VALUE);
+		System.out.println("Maximum range of short: " +Short.MAX_VALUE);
+		System.out.println("Minimum range of Long: " +Long.MIN_VALUE);
+		System.out.println("Maximum range of byte: " +Long.MAX_VALUE);
+		System.out.println("Minimum range of byte: " +Double.MIN_VALUE);
+		System.out.println("Maximum range of byte: " +Double.MAX_VALUE);
+		System.out.println("Minimum range of byte: " +Float.MIN_VALUE);
+		System.out.println("Maximum range of byte: " +Float.MAX_VALUE);
+		System.out.println("Minimum range of byte: " +(int)Character.MIN_VALUE);
+		System.out.println("Minimum range of byte: " +(int)Character.MAX_VALUE);
+		// Note : + datatype.MIN OR MAX_VALUE
+		
+	}
+}
