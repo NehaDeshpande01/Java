cbcd54041b40511bede02ab03c16d7bf310e014a
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-11 16:11:47 +0530

    increment decrement

---
+++ inc.java
@@ -0,0 +1,19 @@
+// increment decrement operator
+package NEW;
+public class inc{
+	public static void main (String [] args) {
+		int a = 5;
+		//post increment
+		System.out.println(a++ +" the value of  a post increment");
+		System.out.println(a);
+		System.out.println(++a +" value of a  pre increment");
+		System.out.println(a);
+		int e = 89;
+		System.out.println(e--  +" the value of e post decrement");
+		System.out.println(e);
+		System.out.println(--e +" the value of e pre dercrement");
+		System.out.println(e);
+		
+		
+	}
+}
