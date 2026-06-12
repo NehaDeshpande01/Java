c7345133439e1535e82c64169e3e928b71d3d7df
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-12 15:24:20 +0530

    state

---
+++ state1.java
@@ -0,0 +1,13 @@
+//if else
+package NEW;
+public class state1{
+	public static void main(String[]args) {
+		int n = 16;
+		if(n%2==0) {
+			System.out.println("it is even");
+		}else {
+			System.out.println("it is odd");
+		}
+	}
+	
+}
