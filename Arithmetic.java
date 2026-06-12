85e5c9372debd7f7d33097a06ecd77577acd9234
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-12 15:25:33 +0530

    arithmetic

---
+++ Arithmetic.java
@@ -0,0 +1,17 @@
+package NEW;
+public class Arithmetic{
+	public static void main(String[]args) {
+		//addition
+		int a = 50;
+		int b = 20;
+		int c = a+b;
+		System.out.println("c " + c);
+		int d = a-b;
+		System.out.println("d " + d);
+		int e = a*b;
+		System.out.println("e " + e);
+		double f = a/b;
+		System.out.println("f " + f);
+		
+	}
+}
