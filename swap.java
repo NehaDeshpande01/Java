dd44afdccafc9a9cb3ca518095913a99fc750abe
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-12 15:21:39 +0530

    swapping

---
+++ swap.java
@@ -0,0 +1,12 @@
+package NEW;
+public class swap{
+	public static void main(String[]args) {
+		int m=20;
+		int n = 50;
+	    m=m+n;//20+50=70
+	    n=m-n;//70-50=20
+	    m=m-n;//70-20=50
+	    System.out.println("value of m after swap:"+m);
+	    System.out.println("value of n after swap:"+n);
+	}
+}
