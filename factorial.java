8d2b43ff052db6669a018ec4ea48daa92c7c1397
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-13 16:49:19 +0530

    factorial

---
+++ factorial.java
@@ -0,0 +1,13 @@
+package NEW;
+// to find factorial number
+public class factorial{
+	public static void main(String[]args) {
+		int a=5;
+		int var=1;
+		for(int i=1;i<=a;i++) {
+			var=var*i;//1*1=1,2,6,24,120
+			
+		}
+		System.out.println(var);
+	}
+	}
