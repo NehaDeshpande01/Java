579ae0ee7858f9266bd17a4019ea2f6ccf33e0d4
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-13 12:52:30 +0530

    while ioop

---
+++ wh1.java
@@ -0,0 +1,16 @@
+package NEW;
+//while loop example print i after true
+
+public class wh1{
+public static void main(String[]args) {
+	System.out.println("starts");
+	int i = 1;
+	while(i<=20)
+	{
+		System.out.println(i);
+		i++;
+	}
+	System.out.println("ends");
+	
+}
+}
