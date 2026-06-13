fe87f0d8d9362916a8f8023a6e3390b6ae317c10
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-13 12:52:04 +0530

    for loop

---
+++ f2.java
@@ -0,0 +1,12 @@
+package NEW;
+//for loop example print table of 23
+
+public class f2{
+public static void main(String[]args) {
+	int num=23;
+	for(int i=1;i<=10;i++) {
+		System.out.println(+num+ "*" +i+ "=" +num*i);
+	}
+	
+}
+}
