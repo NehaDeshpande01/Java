3222c4cce822e3888aa885573de48d8db8a00a2a
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-16 16:26:17 +0530

    myMethod

---
+++ myMethod.java
@@ -0,0 +1,12 @@
+package NEW;
+public class myMethod{
+	public static void main(String[]args) {
+		myMethod("NEHA");
+		myMethod("DESHPANDE");
+		myMethod("TYEJ");
+	
+}
+public static void myMethod(String name) {
+	System.out.println(name);
+}
+}
