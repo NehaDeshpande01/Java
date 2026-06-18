4cd090feaacba8b02174e059afa4092ced6e0797
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-18 17:23:48 +0530

    area.java

---
+++ a2.java
@@ -0,0 +1,10 @@
+// to find area of circle
+package NEW;
+public class a2{
+	public static void main(String[]args) {
+		double rad = 20;
+		double pi = 3.14;
+		double area = pi * rad * rad;
+		System.out.println("Area of circle is:"+area);
+	}
+}
