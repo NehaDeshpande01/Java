891a99da7e3e71185e218616f072513d04f8b614
NehaDeshpande01 <nehadeshpande2809@gmail.com>
2026-06-18 17:24:12 +0530

    Ascii

---
+++ As.java
@@ -0,0 +1,17 @@
+// to find ascii values 
+package NEW;
+public class As{
+	public static void main(String[]args) {
+		int value = 65;
+		for(char i ='A';i<='Z';i++) {
+			System.out.println(i+ "=" +value++);
+		}int value1=97;
+		for(char m='a';m<='z'; m++) {
+			System.out.println(m+ "=" +value1++);
+		}int value2=48;
+		for(int b=0;b<=9;b++) {
+			System.out.println(b+ "=" +value2++);
+		}
+		
+		}
+	}
